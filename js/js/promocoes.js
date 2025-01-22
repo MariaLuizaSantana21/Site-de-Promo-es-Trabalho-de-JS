83f77cb5134f978d8ac3c45f45c21518b69b115f
Maria <luiza.santana@academico.ifpb.edu.br>
2025-01-22 15:46:46 -0300

    fix: change path

--- js/js/promocoes.js
+++ js/js/promocoes.js
@@ -7,7 +7,7 @@ function initApp() {
     let newDiv = document.createElement('div');
     newDiv.classList.add('item');
     newDiv.innerHTML = `
-            <img src="/js/imag/${value.imagem}">
+            <img src="./imag/${value.imagem}">
             <div class="title">${value.title}</div>
             <div class="atualprice">${value.atualprice.toLocaleString()}</div>
             <div class="price">${value.price.toLocaleString()}
