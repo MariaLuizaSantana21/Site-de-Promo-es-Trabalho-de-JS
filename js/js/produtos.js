134303fb3d29a1467164b265b41fee7891a63449
LuizaSantana21 <luiza.santana@academico.ifpb.edu.br>
2025-01-22 09:07:11 -0300

    ajuste estético

--- js/js/produtos.js
+++ js/js/produtos.js
@@ -1,44 +1,44 @@
 export const produtos = [
     {
       id: 1,
-      title: 'Produto 1',
+      title: 'Café com Leite',
       imagem: 'img1.jpg',
+      description:'',
+      price: 'R$ 10,00'
+    },
+    {
+      id: 2,
+      title: 'Chocolate Quente Tradicional',
+      imagem: 'img2.jpg',
       description:'',
       price: 'R$ 15,00'
     },
     {
-      id: 2,
-      title: 'Produto 2',
-      imagem: 'img2.jpg',
-      description:'',
-      price: 'R$ 25,00'
-    },
-    {
       id: 3,
-      title: 'Produto 3',
+      title: 'Brownie com sorvete de creme',
       imagem: 'img3.jpg',
       description:'', 
-      price: "R$ 35,00"
+      price: "R$ 20,00"
     },
     {
       id: 4,
-      title: 'Produto 4',
+      title: 'Sanduiche Misto',
       imagem: 'img4.png',
       description:'',
-      price: "R$ 18,00"
+      price: "R$ 8,00"
     },
     {
       id: 5,
-      title: 'Produto 5',
+      title: 'Petit Gâteau com sorvete de creme',
       imagem: 'img5.jpg',
       description:'',
-      price: "R$ 22,00"
+      price: "R$ 25,00"
     },
     {
       id: 6,
-      title: 'Produto 6',
+      title: 'Croissants Tradicional',
       imagem: 'img6.jpg',
       description:'',
-      price: 'R$ 26,00'
+      price: 'R$ 16,00'
     }
   ];
