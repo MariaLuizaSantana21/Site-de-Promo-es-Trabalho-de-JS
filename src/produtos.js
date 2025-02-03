f56f121cb215931f6144cc264d32aa1b9bb2a6bc
Domilsonsena <domilsonjp@gmail.com>
2025-02-03 14:06:31 -0300

    codigo com vite

---
+++ src/produtos.js
@@ -0,0 +1,50 @@
+export const produtos = [
+  {
+    id: 1,
+    title: 'Café com Leite',
+    imagem: 'img1.jpg',
+    description:'',
+    atualprice: 'R$ 8,00',
+    price: 'R$ 10,00'
+  },
+  {
+    id: 2,
+    title: 'Chocolate Quente Tradicional',
+    imagem: 'img2.jpg',
+    description:'',
+    atualprice: 'R$ 10,00',
+    price: 'R$ 15,00'
+  },
+  {
+    id: 3,
+    title: 'Brownie com sorvete de creme',
+    imagem: 'img3.jpg',
+    description:'', 
+    atualprice: 'R$ 12,00',
+    price: "R$ 20,00"
+  },
+  {
+    id: 4,
+    title: 'Sanduiche Misto',
+    imagem: 'img4.png',
+    description:'',
+    atualprice: 'R$ 5,00',
+    price: "R$ 8,00"
+  },
+  {
+    id: 5,
+    title: 'Petit Gâteau com sorvete de creme',
+    imagem: 'img5.jpg',
+    description:'',
+    atualprice: 'R$ 15,00',
+    price: "R$ 25,00"
+  },
+  {
+    id: 6,
+    title: 'Croissants Tradicional',
+    imagem: 'img6.jpg',
+    description:'',
+    atualprice: 'R$ 12,00',
+    price: 'R$ 16,00'
+  }
+];
---
+++ src/promocoes.js
@@ -0,0 +1,54 @@
+import { produtos } from './produtos';
+let list = document.querySelector(".list"); // Seleciona a lista usando querySelector
+
+function initApp() {
+    produtos.forEach((value, index) => {
+        let newLi = document.createElement('div'); // Cria elementos <li>
+        newLi.classList.add('item');
+        newLi.innerHTML =  `
+            <img src="./src/img ${value.imagem}" 
+            <div class="title">${value.title}</div>
+            <div class="atualprice">${value.atualprice.toLocaleString()}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            `;
+        list.appendChild(newLi); // Adiciona os <li> à lista
+    });
+}
+
+initApp();  
+
+
+const pesquisaInput = document.getElementById('pesquisa');
+
+function exibirProdutos(produtosParaExibir) {
+    list.innerHTML = ''; // Limpa a lista *corretamente*
+
+    produtosParaExibir.forEach(produto => { // Use forEach para iterar e adicionar os elementos
+        
+        const item = document.createElement('div');
+        item.classList.add('item');
+        item.innerHTML =  `
+            <img src="./src/img ${produto.imagem}" 
+            <div class="title">${produto.title}</div>
+            <div class="atualprice">${produto.atualprice.toLocaleString()}</div>
+            <div class="price">${produto.price.toLocaleString()}</div>
+            `;
+
+        list.appendChild(item); // Adiciona os <li> à lista
+    });
+}
+
+function filtrarProdutos() {
+    const termoPesquisa = pesquisaInput.value.toLowerCase();
+
+    const produtosFiltrados = produtos.filter(produto => {
+        return produto.title.toLowerCase().includes(termoPesquisa) || produto.atualprice.toLowerCase().includes(termoPesquisa); // converte o preço para lowerCase tambem
+    });
+
+    exibirProdutos(produtosFiltrados);
+}
+
+exibirProdutos(produtos); // Exibe todos os produtos inicialmente
+
+pesquisaInput.addEventListener('input', filtrarProdutos);
+
